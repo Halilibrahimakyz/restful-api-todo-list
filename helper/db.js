78302d4577a971447572dc496cf0410a1cd11a39
Halil İbrahim AKYÜZ <58sbag58@gmail.com>
2023-03-08 22:25:42 +0300

    Update db.js

--- helper/db.js
+++ helper/db.js
@@ -1,7 +1,7 @@
 const mongoose =require('mongoose');
 
 module.exports=()=>{
-    mongoose.connect('mongodb+srv://halilibrahimakyz:abcd1234@cluster0.646kx.mongodb.net/restful-api',{useNewUrlParser: true,useUnifiedTopology: true});
+    mongoose.connect('mongodb+srv://user',{useNewUrlParser: true,useUnifiedTopology: true});
     mongoose.connection.on('open',()=>{
         console.log('mongoDB:Connected');
     });
